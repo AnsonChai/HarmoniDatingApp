faebb260c97da7f3ccc514e3383e06450e5ca108
FARHIN BIN AHMAD PRIHATIN 2404970B <2404970B@student.tp.edu.sg>
2026-01-20 01:59:11 +0800

    Added Identity

---
+++ HarmoniDatingApp/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+﻿using HarmoniDatingApp.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace HarmoniDatingApp.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<HarmoniDatingAppUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<HarmoniDatingAppUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
---
+++ HarmoniDatingApp/Data/IdentityContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using HarmoniDatingApp.Data;
+
+namespace HarmoniDatingApp.Data
+{
+    public class IdentityContext(DbContextOptions<IdentityContext> options) : IdentityDbContext<HarmoniDatingAppUser>(options)
+    {
+    }
+}
